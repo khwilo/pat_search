c45370af37520415f56990d79c944e4e242a8da1
khwilo <kwhyzzy@gmail.com>
2017-04-22 13:19:10 +0300

    Test for the presence of title

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,23 +1,31 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+	def setup
+		@base_title = "PatSearch"
+	end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help | #{@base_title}"
   end
 	
 	test "should get about" do
 		get static_pages_about_url
 		assert_response :success
+		assert_select "title", "About | #{@base_title}"
 	end
 
 	test "should get contact" do
 		get static_pages_contact_url
 		assert_response :success
+		assert_select "title", "Contact | PatSearch"
 	end
 end
