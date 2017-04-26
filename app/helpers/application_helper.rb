a60394f5ecf081747acc241f3a65043bf5672225
khwilo <kwhyzzy@gmail.com>
2017-04-27 01:19:44 +0300

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+	# Return the full title on per-page basis
+	def full_title(page_title = '')
+		base_title = "PatSearch"
+		if page_title.empty?
+			base_title
+		else
+			page_title + " | " + base_title
+		end
+	end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -13,9 +13,10 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "#{@base_title}"
   end
 
+	
   test "should get help" do
     get static_pages_help_url
     assert_response :success
