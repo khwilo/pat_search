45320a12bb72208ed0e095f94689354ca4031915
khwilo <kwhyzzy@gmail.com>
2017-04-29 15:31:58 +0300

    Downcase email attribute to ensure uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+	# before_save { self.email = email.downcase }
+	before_save { email.downcase! } # An alternative callback implementation
 	validates :name,  presence: true, length: { maximum: 50 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 	validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
