e87664319d37800abe6314c5f8ee7d144f0c85fe
khwilo <kwhyzzy@gmail.com>
2017-05-01 15:13:12 +0300

    Test for invalid submission

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+	test "invalid signup information" do
+		get signup_path
+		assert_select 'form[action="/signup"]'
+		assert_no_difference 'User.count' do
+			post signup_path, params: { user: { name: "", email: "user@invalid", password: "foo", password_confirmation: "bar" }}
+		end
+		assert_template 'users/new'
+		assert_select 'div#error_explanation'
+		assert_select 'div.field_with_errors'
+	end
+end
