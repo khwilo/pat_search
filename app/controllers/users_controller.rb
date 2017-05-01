3c7a91af253a4f4883525ff5fdebafebca1a2e2b
khwilo <kwhyzzy@gmail.com>
2017-05-01 15:34:06 +0300

    Show message upon successful signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
-			# Handle unsuccessful save
+			flash[:success] = "Welcome to PATSEARCH!"
+			redirect_to @user
 		else
 			render 'new'
 		end
