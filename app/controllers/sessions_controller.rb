7724ee37f4e1257c2fffb0003bfefd138f3f86fc
khwilo <kwhyzzy@gmail.com>
2017-05-03 06:58:14 +0300

    Handle invalid form submission

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
-  def new
+	def new
   end
+  
+  def create
+  	user = User.find_by(email: params[:session][:email].downcase)
+  	if user && user.authenticate(params[:session][:password])
+  		# Log in and display relevant page
+  	else
+  		# create an error message
+  		flash.now[:danger] = 'Invalid email/password combination'
+  		render 'new'
+		end
+	end
+
+	def destroy
+	end
 end
